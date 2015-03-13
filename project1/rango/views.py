b14b730194e6284f9954e3e946883c2f1438f0e3
Jahanzeb Farrukh <jfarrukh7000@hotmail.com>
2015-03-13 14:44:33 +0000

    Chapter 15- Completed

--- project1/rango/views.py
+++ project1/rango/views.py
@@ -10,6 +10,7 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
 from datetime import datetime
+from rango.bing_search import run_query
 
 
 def add_category(request):
@@ -143,6 +144,19 @@ def add_page(request, category_name_slug):
 
 
 from rango.forms import UserForm, UserProfileForm
+
+def search(request):
+
+    result_list = []
+
+    if request.method == 'POST':
+        query = request.POST['query'].strip()
+
+        if query:
+            # Run our Bing function to get the results list!
+            result_list = run_query(query)
+
+    return render(request, 'rango/search.html', {'result_list': result_list})
 
 def register(request):
     if request.session.test_cookie_worked():
