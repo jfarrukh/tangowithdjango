73d5df16e836c1417d5df2d7e57f91e0a53c2e78
Jahanzeb Farrukh <jfarrukh7000@hotmail.com>
2015-03-13 13:20:16 +0000

    Chapter 9- Completed

--- project1/project1/settings.py
+++ project1/project1/settings.py
@@ -84,6 +84,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
+LOGIN_URL = '/rango/login/'
 STATIC_URL = '/static/'
 
 
--- project1/rango/models.py
+++ project1/rango/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.template.defaultfilters import slugify
+from django.contrib.auth.models import User
+
 
 # Create your models here.
 class Category(models.Model):
@@ -23,3 +25,19 @@ class Page(models.Model):
 
     def __unicode__(self):
         return self.title
+
+
+
+class UserProfile(models.Model):
+     # This line is required. Links UserProfile to a User model instance.
+     user = models.OneToOneField(User)
+
+
+     # The additional attributes we wish to include.
+     website = models.URLField(blank=True)
+     picture = models.ImageField(upload_to='profile_images', blank=True)
+
+
+     # Override the __unicode__() method to return out something meaningful!
+     def __unicode__(self):
+         return self.user.username
